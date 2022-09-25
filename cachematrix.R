7f94dbe1c14dc6379a15cc6f0c225e6fb83aaed9
Science18 <70443636+Sciencebeing18@users.noreply.github.com>
2022-09-25 22:59:55 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##makeCacheMatrix contains set, get, setinv and getinv
+##library MASS is used to calculate squared and non squared matrices
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+    inv <- NULL    #inverse as null
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x   #this function gets matrix x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() {
+                          inver <-ginv(x)
+                          inver %*% x          #this function helps obtain the inverse of the matrix 
+    }
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## used to describe the cache data
+cacheSolve <- function(x, ...) #this helps get cache data
+  {
+  inv <- x$getinv()
+  if(!is.null(inv)) {             #checks whether the inverse is null
+                     message("getting cached data!")
+                      return(inv)   #returns inverse value
+                     }
+  data <- x$get()
+  inv <- solve(data, ...)                 #calculates inverse values
+  x$setinv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
